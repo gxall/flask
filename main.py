5f5a93cc7e390efead6f3e0039e04f7f81d52309
gxall <113332351+gxall@users.noreply.github.com>
2023-04-03 21:55:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import requests
-import json
+import base64
 
 app = Flask(__name__)
 
-@app.route('/nf_crypto_payment', methods=['POST'])
-def nf_crypto_payment():
-    data = request.get_json()
-    amount = data['amount']
-    telegram_user_id = data['telegram_user_id']
-    data = {"id": "", "price": amount, "currency": "USD", "store_id": "RUZcUraKvqVNWdKCMGreKFrWraLNIbtI", "products": "", "expiration": "", "promocode": "",
-            "notification_url": "https://gcbot-production.up.railway.app/nfwcp", "redirect_url": "", "buyer_email": "",
-            "order_id": "", "discount": "", "status": "", "tx_hashes": "", "exception_status": "",
-            "shipping_address": "", "notes": "", "created": "", "payments": "", "metadata": {
-            "tg_id": telegram_user_id
-        }, "action": ""}
+@app.route('/maxicode', methods=['POST'])
+def maxicode():
+    if request.is_json:
+        data = request.get_json()
+        imgdata = create_maxicode(data)
+        return imgdata
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
+
+def encode_data(data):
+    data = data['maxicode']
+    data = data.replace("\\x1e", "%5Cx1e")
+    data = data.replace("\\F", "%5CF")
+    data = data.replace("\\F0", "%5CF0")
+    data = data.replace("\\F1", "%5CF1")
+    data = data.replace("\\F4", "%5CF4")
+    data = data.replace("\\FN", "%5CFN")
+    data = data.replace("\\FB", "%5CFB")
+    data = data.replace("+", "%2B")
+    data = data.replace(" ", "+")
+    return "data=" + data + "&code=MaxiCode&translate-esc=true&dpi=600&imagetype=Png&base64=true"
+
+
+def create_maxicode(data):
+    url = "https://barcode.tec-it.com/barcode.ashx"
+
+    data = encode_data(data=data)
 
     headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-
+        'authority': 'barcode.tec-it.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'cookie': '__RequestVerificationToken=EpNxhZXq7xYAYIzWh1144QIYmJwYp7sQhsVMEqQqz1C3612Uh6TljNinr-cxY2PmEkuDzwjykiCb2QAqBSlLeLGig1yTRwKf8MbYhYcF8781',
+        'origin': 'https://barcode.tec-it.com',
+        'pragma': 'no-cache',
+        'referer': 'https://barcode.tec-it.com/en/MaxiCode?data=This%20is%20a%20MaxiCode%20by%20TEC-IT',
+        'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="112", "Google Chrome";v="112"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 11.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5529.219 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
     }
-    url = "http://45.153.240.217/api/invoices"
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    r = response
-    print("curl -X GET '" + url + "' -v -H 'User-Agent: " + r.request.headers['User-Agent'] + "' -H 'Accept: " +
-          r.request.headers['Accept'] + "' -H 'Accept-Encoding: " + r.request.headers[
-              'Accept-Encoding'] + "' -H 'Connection: " + r.request.headers['Connection'] + "' -H 'Cache-Control: " +
-          r.request.headers['Cache-Control'] + "'")
 
-    return jsonify({
-        "price_amount": amount,
-        "invoice_url": f"http://c8bc-37-19-221-160.ngrok.io/invoice/{response.json()['id']}",
-        "payments": response.json()['payments']
-    })
+    response = requests.post(url, headers=headers, data=data)
+
+    # Decode the Base64 string
+    imgdata = base64.b64decode(response.text)
+
+    return imgdata
 
 if __name__ == '__main__':
     app.run()
